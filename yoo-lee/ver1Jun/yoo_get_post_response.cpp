3a0ab0b563bfb976be2f46f8430e9bade1af498c
yoo-lee <83053630+yoo-lee@users.noreply.github.com>
2022-12-25 23:11:51 +0900

    +++

--- yoo-lee/ver1Jun/yoo_get_post_response.cpp
+++ yoo-lee/ver1Jun/yoo_get_post_response.cpp
@@ -1,11 +1,48 @@
-class HttpR
+namespace server 
 {
-	
+class ConnSocket;
+}
+//epoll 等
+
+class HttpResponse 
+{
+protected:
+  // レスポンスの種類
+enum EResponseType
+{ kHttpResponse, kHttpErrorResponse, kHttpCgiResponse };
+
+  // レスポンスの作成状況
+  enum CreateResponsePhase
+  {
+    kExecuteRequest,
+    kStatusAndHeader,
+    kBody,
+    kComplete
+  };
+
+  CreateResponsePhase ExecuteGetRequest(const http::HttpRequest &request);
+  CreateResponsePhase ExecutePostRequest(const server::ConnSocket *conn_sock, const http::HttpRequest &request);
+  CreateResponsePhase ExecuteDeleteRequest(const http::HttpRequest &request);
+}
+// ------       ↑header↑        --------------
+
+HttpResponse::HttpResponse(const config::LocationConf *location, server::Epoll *epoll, EResponseType response_type)
+    : location_(location),
+      epoll_(epoll),
+      phase_(kExecuteRequest),
+      http_version_(kDefaultHttpVersion),
+      status_(OK),
+      status_message_(StatusCodes::GetMessage(OK)),
+	  //303 etc
+      headers_(),
+      write_buffer_(),
+      file_fd_(-1),
+      response_type_(response_type) {assert(epoll_ != NULL);
 }
 
-
-HttpResponse::CreateResponsePhase HttpResponse::ExecuteRequest(
-    server::ConnSocket *conn_sock) {
+HttpResponse::CreateResponsePhase
+HttpResponse::ExecuteRequest(server::ConnSocket *conn_sock)
+{
   http::HttpRequest &request = conn_sock->GetRequests().front();
   const std::string method = request.GetMethod();
 
---
+++ yoo-lee/ver1Jun/yoo_get_post_response.h
@@ -0,0 +1,4 @@
+namespace server 
+{
+class ConnSocket;
+}
