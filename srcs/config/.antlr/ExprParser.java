1ce198fcdd755e15526061e1bfec8975fdf0d8d4
yueyama <yueyama@student.42tokyo.jp>
2023-04-15 10:40:08 +0900

    rm sample

--- srcs/config/.antlr/ExprParser.java
+++ srcs/config/.antlr/ExprParser.java
@@ -1,4 +1,4 @@
-// Generated from /home/cou/projects/42/webserv/srcs/config/sample/Expr.g4 by ANTLR 4.9.2
+// Generated from /home/cou/projects/42/webserv/srcs/config/Expr.g4 by ANTLR 4.9.2
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;
