44429c1469f23e8089df503388e7523d05741b10
hsano <hsano@student.42tokyo.jp>
2023-04-20 01:38:00 +0900

    move header file in include dir

---
+++ include/socket.hpp
@@ -0,0 +1,49 @@
+#ifndef SOCKET_HPP
+#define SOCKET_HPP
+
+#include "request.hpp"
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+#include <sys/epoll.h>
+//#include <sys/types.h>
+//#include <netinet/in.h>
+//
+//
+
+typedef struct clientinfo{
+    int fd;
+    char buf[1024];
+    int n;
+    int state;
+}s_clientinfo;
+class Socket
+{
+    public:
+        Socket();
+        Socket(std::string port);
+        //Socket(const Socket &socket);
+        Socket(const Socket &socket);
+        Socket& operator=(const Socket &socket);
+        virtual ~Socket();
+        virtual void communication();
+        void close_fd();
+        int getSockFD();
+        Request *recv();
+        //Request *recv(int fd);
+        bool send(std::string& data);
+        int accept_request();
+    protected:
+        int sock_fd;
+        int fd;
+        std::string port;
+        virtual void init();
+        virtual int makeSocket();
+        virtual void setAddrInfo(struct addrinfo &info);
+        Request *req;
+        struct epoll_event ev;
+        s_clientinfo clientinfo;
+        //struct sockaddr_in hint;
+};
+
+#endif
---
+++ include/webserv.hpp
@@ -0,0 +1,29 @@
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
+#include <vector>
+#include "tcp_socket.hpp"
+
+class Webserv
+{
+    public:
+        Webserv();
+        Webserv(const std::vector<std::string> ports);
+        //Webserv(size_t size);
+        //Webserv(const Webserv &socket);
+        Webserv(const Webserv &sockets);
+        Webserv& operator=(const Webserv &sockets);
+        ~Webserv();
+        void communication();
+        bool change_epoll_config_to_write(int fd, int event);
+    private:
+        int epfd;
+        void init_socket(std::vector<std::string>);
+        bool init_epoll();
+        void close_all();
+        void connected_communication(int fd, struct epoll_event *event, Socket *socket);
+        std::vector<Socket*> sockets;
+        Socket* find_listen_socket(int socket_fd);
+        //struct epoll_event ev;
+
+};
+#endif
