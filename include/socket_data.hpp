55a3db0e9c8b72938a174318f5f00110cd68a701
hsano <hsano@student.42tokyo.jp>
2023-05-04 05:07:53 +0900

    add socket_data that is paraent class of Reqeust and Response

---
+++ include/socket_data.hpp
@@ -0,0 +1,39 @@
+#ifndef SOCKET_DATA_HPP
+#define SOCKET_DATA_HPP
+
+#include <map>
+#include <string>
+#include "split.hpp"
+#include "get_next_line.hpp"
+
+enum E_METHOD{
+    GET,
+    POST,
+    PUT,
+    HEAD,
+    DELETE,
+    OPTIONS,
+    TRACE,
+    CONNECT,
+    NG,
+    INVALID,
+};
+
+typedef enum E_METHOD METHOD;
+
+class SocketData
+{
+    public:
+        SocketData();
+        ~SocketData();
+
+        static std::string identify_method(METHOD method);
+        static METHOD identify_method(std::string method);
+        bool increment_timeout(int time);
+        void clear_timeout();
+    protected:
+        const static int _timeout = 200;
+        int _timeout_cnt;
+};
+
+#endif
