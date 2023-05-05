b200bd38bfffe143a9bdbc9a42d871efa4147c2e
hsano <hsano@student.42tokyo.jp>
2023-05-05 13:44:16 +0900

    delete obj

--- obj/socket.d
+++
@@ -1,7 +0,0 @@
-obj/socket.o: srcs//socket.cpp include/tcp_socket.hpp include/socket.hpp \
- include/request.hpp include/split.hpp include/get_next_line.hpp
-include/tcp_socket.hpp:
-include/socket.hpp:
-include/request.hpp:
-include/split.hpp:
-include/get_next_line.hpp:
--- obj/tcp_socket.d
+++
@@ -1,8 +0,0 @@
-obj/tcp_socket.o: srcs//tcp_socket.cpp include/tcp_socket.hpp \
- include/socket.hpp include/request.hpp include/split.hpp \
- include/get_next_line.hpp
-include/tcp_socket.hpp:
-include/socket.hpp:
-include/request.hpp:
-include/split.hpp:
-include/get_next_line.hpp:
