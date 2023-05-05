7edccd029be825384e536dc7a00fa3648551a4dd
yueyama <yueyama@student.42tokyo.jp>
2023-05-05 22:50:43 +0900

    refactoring: reformatting

--- srcs/request.cpp
+++ srcs/request.cpp
@@ -1,32 +1,39 @@
 #include "request.hpp"
-#include "split.hpp"
 #include "get_next_line.hpp"
-#include <string>
-#include <unistd.h>
-#include <iostream>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <algorithm>
-#include <sstream>
+#include "split.hpp"
 #include "utility.hpp"
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
 using std::map;
+using std::string;
 
-Request::Request(int fd_) : fd(fd_), _content_length(0), _loaded_body_size(0), _gnl(this->fd) , method(NG),  err_line(""),  _data_in_body(false), _cgi(false)
+Request::Request(int fd_)
+    : fd(fd_),
+      _content_length(0),
+      _loaded_body_size(0),
+      _gnl(this->fd),
+      method(NG),
+      err_line(""),
+      _data_in_body(false),
+      _cgi(false)
 {
     this->parse();
 }
 
 Request::~Request()
 {
-    //delete _body;
-    //close(this->fd);
+    // delete _body;
+    // close(this->fd);
 }
-
 
 void Request::print_request()
 {
@@ -35,12 +42,12 @@ void Request::print_request()
     cout << "uri: " << this->uri << endl;
     cout << "version: " << this->version << endl;
 
-    cout << "headers size:" << this->headers.size() << endl;;
+    cout << "headers size:" << this->headers.size() << endl;
+    ;
     map<string, string>::iterator ite = this->headers.begin();
     map<string, string>::iterator end = this->headers.end();
     int i = 0;
-    for(; ite != end; ite++)
-    {
+    for (; ite != end; ite++) {
         cout << (*ite).first << ":" << (*ite).second << endl;
         i++;
     }
@@ -49,11 +56,11 @@ void Request::print_request()
 void Request::parse()
 {
     string str = _gnl.getline();
-    if (str == _gnl.last_str){
+    if (str == _gnl.last_str) {
         return;
     }
     Split sp(str, " ");
-    if (sp.size() != 3){
+    if (sp.size() != 3) {
         cout << "size:" << sp.size() << endl;
         cout << "str:[" << str << "]" << endl;
         str = _gnl.getline();
@@ -64,10 +71,10 @@ void Request::parse()
     Split::iterator ite = sp.begin();
     this->method = identify_method(*ite);
     this->uri = *(++ite);
-    const char *path = this->uri.c_str();
+    const char* path = this->uri.c_str();
     size_t cnt = 0;
-    while (path && *path){
-        if(*path != '/')
+    while (path && *path) {
+        if (*path != '/')
             break;
         cnt++;
         path++;
@@ -79,31 +86,29 @@ void Request::parse()
     string header;
     string value;
     std::string::size_type pos;
-    while((str != _gnl.last_str)) {
+    while ((str != _gnl.last_str)) {
         str = _gnl.getline();
         pos = str.find(":");
-        if (pos == string::npos || str.size() <= 0)
-        {
+        if (pos == string::npos || str.size() <= 0) {
             break;
         }
         header = str.substr(0, pos);
         header = Utility::delete_space(header);
         header = Utility::delete_space(header);
-        if(header.size() < 2)
-        {
+        if (header.size() < 2) {
             break;
         }
         std::transform(header.begin(), header.end(), header.begin(), tolower);
-        value = str.substr(pos+1);
+        value = str.substr(pos + 1);
         value = Utility::delete_space(value);
         this->headers.insert(make_pair(header, value));
     }
     string size_str = this->search_header("content-length");
     ssize_t size = -1;
-    if (size_str.size() > 10){
+    if (size_str.size() > 10) {
         cout << "Error: exceed BODY SIZE MAX" << endl;
     }
-    if (size_str != ""){
+    if (size_str != "") {
         std::stringstream ss;
         ss << size_str;
         ss >> size;
@@ -119,25 +124,25 @@ METHOD Request::get_method()
 
 const std::string Request::get_method_string()
 {
-	return(identify_method(this->method));
+    return (identify_method(this->method));
 }
 
-const string &Request::get_uri()
+const string& Request::get_uri()
 {
     return (this->uri);
 }
 
-const string &Request::get_version()
+const string& Request::get_version()
 {
     return (this->version);
 }
 
-const map<string, string> &Request::get_headers()
+const map<string, string>& Request::get_headers()
 {
     return (this->headers);
 }
 
-int Request::read_buf(char *buf)
+int Request::read_buf(char* buf)
 {
     int size = this->_gnl.get_extra_buf(buf);
     if (size > 0)
@@ -152,13 +157,13 @@ const string& Request::get_path()
 
 string Request::get_ip_address()
 {
-    //todo
+    // todo
     return ("127.0.0.1");
 }
 
 string Request::get_domain()
 {
-    //todo
+    // todo
     return ("test.com");
 }
 
@@ -187,8 +192,7 @@ string Request::search_header(string header)
     (void)header;
     std::map<std::string, std::string>::const_iterator ite;
     ite = this->headers.find(header);
-    if (ite == this->headers.end())
-    {
+    if (ite == this->headers.end()) {
         return ("");
     }
     return (ite->second);
@@ -211,4 +215,3 @@ bool Request::is_cgi()
 {
     return (false);
 }
-
