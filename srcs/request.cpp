7c25a5e91a352083711c58c6ebc714a1c5a5acf8
yueyama <yueyama@student.42tokyo.jp>
2023-05-05 23:44:27 +0900

    add: Add is_cgi

--- srcs/request.cpp
+++ srcs/request.cpp
@@ -1,4 +1,5 @@
 #include "request.hpp"
+#include "Config.hpp"
 #include "get_next_line.hpp"
 #include "split.hpp"
 #include "utility.hpp"
@@ -213,10 +214,23 @@ bool Request::have_data_in_body()
 
 bool Request::is_cgi()
 {
-    return (false);
+    return is_cgi(this->path);
 }
 
 bool Request::is_cgi(string path) const
 {
-    return true;
+    vector<const Server*> server_list = _config->http->server;
+    for (size_t i = 0; i < server_list.size(); i++) {
+        if (server_list[i]->location.size() == 0)
+            continue;
+        for (size_t j = 0; j < server_list[i]->location.size(); j++) {
+            Location* current = const_cast<Location*>(server_list[i]->location[j]);
+            for (size_t k = 0; k < current->urls.size(); k++) {
+                if (current->urls[k] == path && (*current)["cgi_path"][0] != "") {
+                    return (true);
+                }
+            }
+        }
+    }
+    return (false);
 }
