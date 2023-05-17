e5125932dccec5b2cb7f06e93a3113340adadc02
yueyama <yueyama@student.42tokyo.jp>
2023-05-17 14:16:29 +0900

    test: Added get_default_server() test

--- srcs/config/Config.hpp
+++ srcs/config/Config.hpp
@@ -20,7 +20,7 @@ class Config
     Config(const char* file_path);
     void init(string file_text);
     ~Config();
-    Server get_default_server() const;
+    Server const& get_default_server() const;
 };
 
 #endif /* CONFIG_H */
