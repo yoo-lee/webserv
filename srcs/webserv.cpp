ccea7e0506bdfec2259ff916a24460e4a30cdd26
yueyama <yueyama@student.42tokyo.jp>
2023-05-31 18:00:12 +0900

    remove: prに関係ないところをコメントアウト

--- srcs/webserv.cpp
+++ srcs/webserv.cpp
@@ -165,57 +165,57 @@ void Webserv::process_connected_communication(int fd, struct epoll_event* event,
             res = new Response(*req);
             // cig processing
         } else {
-            string body;
-            string status_code;
-            Location const* l = _config.get_location(req->get_port(), req->get_host(), req->get_path());
-            // if(is_redirect())
-            string path;
-            if (l->root != "")
-                path = l->root + req->get_path();
-            else
-                path = FileUtility::get_cwd() + req->get_path();
-            if (!FileUtility::is_directory_exist(path) && !FileUtility::is_file_exist(path))
-                status_code = "404";
-            else {
-                if (req->get_method() == HttpMethod::GET) {
-                    if (FileUtility::is_file_exist(path)) {
-                        body = FileUtility::read_file_text(path);
-                        status_code = "200";
-                    } else {
-                        if (!l->autoindex) {
-                            status_code = "403";
-                        } else {
-                            try {
-                                body = get_auto_index_page(path);
-                            } catch (exception& e) {
-                                // get_auto_index_pageの返す例外によってstatus_codeを変更
-                                // 仮で500
-                                status_code = "500";
-                            }
-                        }
-                    }
-                } else if (req->get_method() == HttpMethod::POST) {
-                    if (FileUtility::is_directory_exist(path)) {
-                        try {
-                            // create new file
-                        } catch (exception& e) {
-                            // create new fileの返す例外によってstatus_codeを変更
-                            // 仮で500
-                            status_code = "500";
-                        }
-                    } else
-                        status_code = "404";
+            // string body;
+            // string status_code;
+            // Location const* l = _config.get_location(req->get_port(), req->get_host(), req->get_path());
+            // // if(is_redirect())
+            // string path;
+            // if (l->root != "")
+            //     path = l->root + req->get_path();
+            // else
+            //     path = FileUtility::get_cwd() + req->get_path();
+            // if (!FileUtility::is_directory_exist(path) && !FileUtility::is_file_exist(path))
+            //     status_code = "404";
+            // else {
+            //     if (req->get_method() == HttpMethod::GET) {
+            //         if (FileUtility::is_file_exist(path)) {
+            //             body = FileUtility::read_file_text(path);
+            //             status_code = "200";
+            //         } else {
+            //             if (!l->autoindex) {
+            //                 status_code = "403";
+            //             } else {
+            //                 try {
+            //                     body = get_auto_index_page(path);
+            //                 } catch (exception& e) {
+            //                     // get_auto_index_pageの返す例外によってstatus_codeを変更
+            //                     // 仮で500
+            //                     status_code = "500";
+            //                 }
+            //             }
+            //         }
+            //     } else if (req->get_method() == HttpMethod::POST) {
+            //         if (FileUtility::is_directory_exist(path)) {
+            //             try {
+            //                 // create new file
+            //             } catch (exception& e) {
+            //                 // create new fileの返す例外によってstatus_codeを変更
+            //                 // 仮で500
+            //                 status_code = "500";
+            //             }
+            //         } else
+            //             status_code = "404";
 
-                } else /* DELETE */ {
-                    if (FileUtility::is_file_exist(path)) {
-                        FileUtility::delete_file(path);
-                        status_code = "200";
-                    } else /* path is directory */ {
-                        status_code = "405";
-                    }
-                }
-            }
-            std::cout << "status code: " << status_code << std::endl;
+            //     } else /* DELETE */ {
+            //         if (FileUtility::is_file_exist(path)) {
+            //             FileUtility::delete_file(path);
+            //             status_code = "200";
+            //         } else /* path is directory */ {
+            //             status_code = "405";
+            //         }
+            //     }
+            // }
+            // std::cout << "status code: " << status_code << std::endl;
 
             res = new Response(*req);
             // server processing except cgi
